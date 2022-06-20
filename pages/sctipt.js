3a9fca8b5d8eaca2c75f7f32ed08c11682701729
Andrew Stepanov <andrewstepanov@icloud.com>
2022-06-20 08:12:15 +0300

    перенес файлы

---
+++ pages/sctipt.js
@@ -0,0 +1,9 @@
+function clodeEditForm() {
+  var element = document.getElementById("edit-form");
+  element.classList.remove("edit-form_opened");
+}
+
+function openEditForm() {
+  var element = document.getElementById("edit-form");
+  element.classList.add("edit-form_opened");
+}
