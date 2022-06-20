e9f5570bb25f1c0370940138332f28f823fcaca9
Andrew Stepanov <andrewstepanov@icloud.com>
2022-06-20 18:59:24 +0300

    fix

--- pages/sctipt.js
+++
@@ -1,9 +0,0 @@
-function clodeEditForm() {
-  var element = document.getElementById("edit-form");
-  element.classList.remove("edit-form_opened");
-}
-
-function openEditForm() {
-  var element = document.getElementById("edit-form");
-  element.classList.add("edit-form_opened");
-}
